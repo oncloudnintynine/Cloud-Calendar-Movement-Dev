a36b69f734fb36d94062151ad4b3bb30c3220cde
oncloudnintynine <oncloudnintynine@gmail.com>
2026-04-26 16:05:53 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const ENV = 'Dev'; // TOGGLE TO 'Prod' FOR PRODUCTION
 
-const DEV_URL = 'https://script.google.com/macros/s/AKfycbwUaKsHpWkPVpQ7Y0RahYTdIcy6K_Wh6lihPyzo6KmY-f1mdIY-A4cAN41I0YwRGK48gg/exec';
+const DEV_URL = 'https://script.google.com/macros/s/AKfycbzEFd3-Bu1-h1oUKNwz8kEE8qyfMP9KzooIoIszVpL4LDXaSyn_xiPCYJrG_nJfUE2hZQ/exec';
 const PROD_URL = 'https://script.google.com/macros/s/AKfycbw6GmmwAW7UoSpjNoCnkdeAVDHmA0amBu73hy43NOj77KGggTzXeRvOFhpWA_dDE3k7/exec';
 
 const API_URL = ENV === 'Dev' ? DEV_URL : PROD_URL;
