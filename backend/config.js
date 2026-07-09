40e0922fabbacebd2d7fff2bf629668c7ece1bb2
oncloudnintynine <oncloudnintynine@gmail.com>
2026-07-09 13:02:04 +0800

    Automated emergency update via App Code Maintainer

--- backend/config.js
+++ backend/config.js
@@ -7,8 +7,13 @@
 const ENV = 'Dev'; 
 
 // ENVIRONMENT API ENDPOINTS (Google Apps Script Web App URLs)
+// Replace these with your respective deployment URLs
+
 const EXP_URL = 'https://script.google.com/macros/s/AKfycbwgY-ik3-hWmoLIziCkyoP2RZCriI_cZTgLgBWLxx37QZZ_7j8Pu68DwMAKXBUkI46WOQ/exec';
+
+// REPLACE THIS URL AFTER DEPLOYING FROM THE NEW ACCOUNT
 const DEV_URL = 'https://script.google.com/macros/s/AKfycbzEFd3-Bu1-h1oUKNwz8kEE8qyfMP9KzooIoIszVpL4LDXaSyn_xiPCYJrG_nJfUE2hZQ/exec';
+
 const PROD_URL = 'https://script.google.com/macros/s/AKfycbw6GmmwAW7UoSpjNoCnkdeAVDHmA0amBu73hy43NOj77KGggTzXeRvOFhpWA_dDE3k7/exec';
 
 const API_URL = ENV === 'Exp' ? EXP_URL : (ENV === 'Dev' ? DEV_URL : PROD_URL);
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'cloudy-v122';
+const CACHE_NAME = 'cloudy-v123';
 
 const urlsToCache =[
 './',
